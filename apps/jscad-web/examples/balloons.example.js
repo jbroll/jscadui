c23c8600749f69dd77a011a5ca2b22f903aace08
John Roll <john@rkroll.com>
2026-01-26 17:58:18 -0500

    Revert "fix(examples): hardcode segments in balloons example"
    This reverts commit 6460ba6a56fe15e0e0e6f3120b126ac8955a0cac.

--- apps/jscad-web/examples/balloons.example.js
+++ apps/jscad-web/examples/balloons.example.js
@@ -68,7 +68,7 @@ const createSingleBalloon = (params) => {
 
   const b = ellipsoid({
     radius: [params.bRadius, params.bRadius, params.bRadius],
-    segments: 32
+    segments: params.segments || 32
   })
   return subtract(b, t)
 }
